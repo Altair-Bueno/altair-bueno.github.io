f5d2d9d7a3bbb211d6353ace6b9c3646ea524662
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-06-28 20:51:45 +0200

    new: replaced spinner and error with components. Added new events component

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,33 @@
 <script lang="ts">
     import InfoCard from "./lib/InfoCard.svelte";
     import Footer from "./lib/Footer.svelte";
+    import Spinner from "./lib/Spinner.svelte";
+    import Error from "./lib/Error.svelte";
+    import Events from "./lib/Events.svelte";
 
-    import spinner from './assets/spinner.svg'
     import config from "./assets/data/config.json"
     import acknowledgments from "./assets/data/acknowledgments.json"
 
-    const {websiteSource,resume} = config
-
-    const resumePromise = fetch(resume.link,resume.request).then(x => x.json())
+    const {websiteSource, resume, events} = config
+    const resumePromise = fetch(resume.link, resume.request).then(x => x.json())
+    const eventsPromise = fetch(events.link, events.request).then(x => x.json())
+    const all = Promise.all([resumePromise, eventsPromise]);
 
 </script>
-<main class="flex flex-col w-full h-screen">
+<div class="flex flex-col w-full h-screen">
   <div class="grid place-content-center mt-24">
-    {#await resumePromise}
-      <img src="{spinner}" alt="" class="invert w-24"/>
-    {:then resume}
-      <InfoCard {resume} {websiteSource}/>
-    {:catch error}
-      <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
-           role="alert">
-        <strong class="font-bold">Heads up!</strong>
-        <span class="block sm:inline">Something went wrong while loading the content</span>
-        <span class="absolute top-0 bottom-0 right-0 px-4 py-3"></span>
-      </div>
-    {/await}
+    <main>
+      {#await all}
+        <Spinner/>
+      {:then [resume, events]}
+        <InfoCard {resume} {websiteSource}/>
+        <Events {events}/>
+      {:catch error}
+        <Error/>
+      {/await}
+    </main>
   </div>
   <footer class="mt-auto w-full">
     <Footer {acknowledgments}/>
   </footer>
-</main>
+</div>
---
+++ src/lib/Events.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import {GitHubEvent} from "../types/event"
+
+    export let events: GitHubEvent[]
+</script>
