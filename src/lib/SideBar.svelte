75d8ac1a28549a71558fcfacc8c39027e3f5b234
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-07-01 20:53:53 +0200

    new: Added support for 2 columns on small screens

--- src/lib/SideBar.svelte
+++ src/lib/SideBar.svelte
@@ -6,9 +6,11 @@
 </script>
 <div class="bg-teal-100 dark:bg-zinc-800 dark:text-gray-50 rounded-2xl p-5 block m-1">
   <h2 class="text-xl">Latest updates</h2>
-  <ul>
+  <ul class="mt-2 grid grid-cols-2">
     {#each events as event (event.id)}
-      <SideBarEvent {event}/>
+      <li class="col-span-1 md:col-span-2">
+        <SideBarEvent {event}/>
+      </li>
     {/each}
   </ul>
 </div>
