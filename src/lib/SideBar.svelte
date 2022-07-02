0b81461f89ca9539790fbcdd5662c6587379555d
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-07-02 18:55:51 +0200

    new: Filter GitHub events and transform them into sidebar elements

--- src/lib/SideBar.svelte
+++ src/lib/SideBar.svelte
@@ -1,15 +1,60 @@
 <script lang="ts">
-    import {GitHubEvent} from "../types/event"
-    import SideBarEvent from "./SideBarEvent.svelte";
+    import {EventType, GitHubEvent} from "../types/event"
+    import SideBarEvent from "./SideBarElement.svelte";
 
-    export let events: GitHubEvent[]
+    export let minElements = 4
+    export let events: GitHubEvent[] = []
+
+    let nupdates: number | undefined = minElements
+
+    interface Update {
+        id: string
+        title: string
+        description: string
+        link: string
+        date: Date
+    }
+
+    const createUpdate = (event, title, description?, link?) => {
+        const id = event.id
+        const date = new Date(event.created_at)
+        //title = "sldkfjj asj kafjk jajsj jasjk fdjas klfjasjdf askfjlasjdfjasl jfajskdl fjaksjf sajdl fj"
+        description = description ? description : event.repo.name
+        link = link ? link : `https://github.com/${event.repo.name}`
+        return {id, title, description, link, date}
+    }
+
+    const strategies = new Map() as Map<EventType, (event: GitHubEvent) => Update>
+    //strategies.set(EventType.PushEvent, (event: GitHubEvent) => createUpdate(event, `Pushed ${event.payload.commits.length} commit${event.payload.commits.length > 1 ? "s" : ""} to ${event.payload.ref.replace("refs/heads/","")}`))
+    strategies.set(EventType.IssuesEvent, (event) => createUpdate(event, event.payload.issue.title, `${event.payload.action.charAt(0).toUpperCase() + event.payload.action.slice(1)} issue`, event.payload.issue.html_url))
+    strategies.set(EventType.PullRequestEvent, (event) => createUpdate(event, event.payload.pull_request.title, `${event.payload.action.charAt(0).toUpperCase() + event.payload.action.slice(1)} pull request`, event.payload.pull_request.html_url))
+
+    function eventToUpdate(event: GitHubEvent) {
+        const strategy = strategies.get(event.type)
+        return strategy(event)
+    }
+
+    function eventFilter(event: GitHubEvent) {
+        return strategies.has(event.type)
+    }
+
+    function toggleAll() {
+        nupdates = nupdates ? undefined : minElements
+    }
+
+    $: updates = events.filter(eventFilter)
+        .slice(0, nupdates)
+        .map(eventToUpdate)
+
 </script>
 <div class="bg-teal-100 dark:bg-zinc-800 dark:text-gray-50 rounded-2xl p-5 block m-1">
-  <h2 class="text-xl">Latest updates</h2>
+  <h2 class="text-xl">
+    Latest updates
+  </h2>
   <ul class="mt-2 grid grid-cols-2">
-    {#each events as event (event.id)}
+    {#each updates as update (update.id)}
       <li class="col-span-1 md:col-span-2">
-        <SideBarEvent {event}/>
+        <SideBarEvent {...update}/>
       </li>
     {/each}
   </ul>
---
+++ src/lib/SideBarElement.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export let title: string
+    export let description: string
+    export let link: string
+    export let date: Date
+
+    $: timestamp = `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`
+
+</script>
+<article class="max-w-md">
+  <h3 class="text-md">
+    <a class="underline text-emerald-300 visited:text-emerald-500 hover:text-emerald-500"
+       href="{link}"
+       target="_blank">
+      {title}
+    </a>
+  </h3>
+  <div class="text-gray-500 text-md">
+    <span>{description}</span>
+    <span>{timestamp}</span>
+  </div>
+</article>
