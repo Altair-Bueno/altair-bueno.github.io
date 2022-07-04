56ee2151361162c5124b67cd0b82d305284a2b14
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-07-04 21:33:14 +0200

    ref: Improved readability on Squircle.svelte

--- src/lib/Squircle.svelte
+++ src/lib/Squircle.svelte
@@ -1,6 +1,7 @@
 <script>
-  $: classes = $$props.class ? $$props.class : ""
+  const classes = 'shadow shadow-xl rounded-2xl p-5 block m-1' + ' ' +
+      'bg-red-200 dark:bg-slate-800 dark:text-gray-50'
 </script>
-<div class="shadow shadow-xl bg-red-200 dark:bg-slate-800 dark:text-gray-50 rounded-2xl p-5 block m-1 {classes}">
+<div class="{classes} {$$props.class || ''}">
   <slot/>
 </div>
