4f5c23fcaeda1b2af774173ec544191f6672792c
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-07-04 16:25:49 +0200

    new: Commit messages are rendered using SvelteMarkdown

--- src/lib/SideBarElement.svelte
+++ src/lib/SideBarElement.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import Link from "./Link.svelte";
+    import SvelteMarkdown from 'svelte-markdown'
 
     export let title: string
     export let description: string
@@ -11,10 +12,14 @@
 </script>
 <article class="max-w-md hover:shadow-2xl hover:bg-red-300 hover:dark:bg-slate-900 p-2 rounded-xl">
   <h3 class="text-md">
-    <Link href="{link}" target="_blank">{title}</Link>
+    <Link href="{link}" target="_blank">
+      <SvelteMarkdown isInline source="{title}"/>
+    </Link>
   </h3>
   <div class="text-gray-800 dark:text-gray-400 text-md flex justify-between">
-    <span>{description}</span>
+    <span>
+      <SvelteMarkdown isInline source="{description}"/>
+    </span>
     <span>{timestamp}</span>
   </div>
 </article>
