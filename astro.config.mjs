708e8700b14a9ec8cc3c7adadcb076eda42507ce
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2023-06-10 20:46:29 +0200

    feat: Asset compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()],
+  integrations: [svelte(), tailwind(), compress()],
 });
